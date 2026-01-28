bbd47dcc20c439b2d640a9137f10c3068022993d
LauGarzon <lauragarzon494025@gmail.com>
2026-01-28 13:56:03 -0500

    Cajero actualizado con bucle while

--- src/LogicaProgramacion.java
+++ src/LogicaProgramacion.java
@@ -1,60 +1,50 @@
 import java.util.Scanner;
 public class LogicaProgramacion {
     public static void main(String[] args) {
+
         Scanner lector = new Scanner(System.in);
 
-        //Declaracion variables
+        //Declaracion Variables
         double saldoDisponible = 500.0;
         int claveCorrecta = 4940;
+        boolean sistemaEncendido = true; //Sistema para que no se cierre
 
         System.out.println("Ingrese clave:");
         int claveIngresada = lector.nextInt();
 
-        // 1. Filtro de Seguridad
         if (claveIngresada == claveCorrecta) {
-            System.out.println("Clave correcta.");
-            System.out.println("Su saldo actual es: $" + saldoDisponible);
 
-            // 2. Menú de opciones
-            System.out.println("\n¿Qué desea hacer?");
-            System.out.println("1. Retirar dinero");
-            System.out.println("2. Salir");
-            System.out.println("3. Consignar dinero");
-            int opcion = lector.nextInt();
+            // Sistema abierto
+            while (sistemaEncendido) {
+                System.out.println("Bienvendido: Escoge una opción");
+                System.out.println("\n MENÚ PRINCIPAL ");
+                System.out.println("Saldo actual: $" + saldoDisponible);
+                System.out.println("1. Retirar");
+                System.out.println("2. Consignar");
+                System.out.println("3. SALIR");
+                int opcion = lector.nextInt();
 
-            if (opcion == 1) {
-                // Funcion para el retiro
-                System.out.println("Ingrese monto a retirar:");
-                double retiro = lector.nextDouble();
-
-                if (retiro <= saldoDisponible) {
-                    saldoDisponible -= retiro;
-                    System.out.println("Retiro exitoso. Nuevo saldo: $" + saldoDisponible);
-                } else {
-                    System.out.println("Fondos insuficientes.");
+                if (opcion == 1) {
+                    System.out.println("¿Cuánto retira?");
+                    double retiro = lector.nextDouble();
+                    if(retiro <= saldoDisponible) saldoDisponible -= retiro;
+                    else System.out.println("Fondos insuficientes");
                 }
-            } else if (opcion == 2) {
-                // Salir4
-                System.out.println("Gracias por consultar su saldo. ¡Vuelva pronto!");
-            } else if (opcion == 3){
-                System.out.println("Ingrese monto a consignar:");
-                double consignar = lector.nextDouble();
-
-                if (saldoDisponible >=consignar){
-                    saldoDisponible += consignar;
-                    System.out.println("Consignacion exitosa");
-                    System.out.println("Su nuevo saldo es:"+ saldoDisponible);
+                else if (opcion == 2) {
+                    System.out.println("¿Cuánto consigna?");
+                    double con = lector.nextDouble();
+                    saldoDisponible += con;
                 }
-            }
-            else {
-                System.out.println("Opción no válida.");
-            }
-            //
+                else if (opcion == 3) {
+
+                    sistemaEncendido = false;
+                    System.out.println("Cerrando sesión... ¡Adiós!");
+                }
+            } // Aquí termina el while
 
         } else {
             System.out.println("Clave incorrecta.");
         }
-
     }
 }
 
